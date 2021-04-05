28ec54b74a1f24048721437ae0d5420152c429ae
Shiva Jabarnia <68242445+jabarnia@users.noreply.github.com>
2021-04-04 21:45:58 -0500

    bokeh chart

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ import datetime
 import streamlit as st
 import yfinance as yf
 import matplotlib.pyplot as plt
+from bokeh.plotting import figure
+from bokeh.models import ColumnDataSource
+import holoviews as hv
 
 
 ##########
@@ -22,42 +25,28 @@ st.write('April 2021 - Shiva Jabarnia')
 
 ticker = st.selectbox('Ticker', ticker_list)
 year = st.selectbox('Year', range(2021,1990, -1))
-month = st.selectbox('Month', range(1, 13))
+month_text = st.selectbox('Month', ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'])
+
+month_num = {'Jan': 1, 'Feb': 2, 'Mar': 3, 'Apr':4, 'May':5, 'Jun':6, 'Jul':7, 'Aug':8, 'Sep':9, 'Oct':10, 'Nov':11, 'Dec':12}
+month = month_num[month_text]
 
 yf_data = yf.Ticker(ticker)
-# get stock info
-st.write('Industry = ' + yf_data.info['industry'])
-# get historical market data
 start = str(year) + '-' + str(month) + '-01'
 if month == 12:
     end = str(year + 1) + '-01-01'
 else:
     end = str(year) + '-' + str(month + 1) + '-01'
 
-if st.button('draw'):
+if st.button('show chart'):
+    st.write('Industry: ' + yf_data.info['industry'])
     df = yf_data.history(start = start, end = end)
     df.reset_index(inplace = True)
     df['Date'] = df['Date'].dt.date
-    df = df[['Date', 'Close']].set_index('Date')
-    st.line_chart(df)
+    df = df[['Date', 'Close']]
+    #st.line_chart(df)
     
+    source = ColumnDataSource(df)
 
-# app = Flask(__name__)
-# # key = 93B6GO8P43HIO48E
-
-
-# @app.route('/')
-# def index():
-#   return render_template('index.html')
-
-# @app.route('/about')
-# def about():
-#   return render_template('about.html')
-
-# if __name__ == '__main__':
-#   app.run(port=33507)
-
-
-
-
-
+    p = figure(title = ticker + ' stock Close price during ' + month_text + ' of year ' + str(year), x_axis_label = 'x' , y_axis_label = 'y')
+    p.line(x = 'Date', y = 'Close', source = source , line_width = 2)
+    st.bokeh_chart(p, use_container_width = True)
